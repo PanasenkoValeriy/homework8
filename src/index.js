b6da67cc5a14012443ff002987732eb320b5312d
PanasenkoValeriy <124078834+PanasenkoValeriy@users.noreply.github.com>
2024-09-02 16:30:42 +0200

    up

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// 1
+// import book from './book.json';
+
+// try {
+//   const bookObject = JSON.stringify(book);
+//   console.log('is valid', bookObject);
+// } catch (error) {
+//   console.error('invalid', error);
+// }
+
+// 2
+// const form = document.getElementById('user-form');
+
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(e) {
+//     e.preventDefault();
+//     const formData = {
+//     name: e.target.name.value,
+//     age: e.target.age.value,
+//     }
+
+// const stringFormData = JSON.stringify(formData)
+
+//     try {
+//     const parsedFormbata = JSON.parse(stringFormData);
+//     console.log('Is valid:', parsedFormData);
+//     } catch (error) {
+//         console.error('Is invalid', error);
+// }
+// }
+
+// 3
+// import movies from './movies.json';
+
+// const stringifiedMovies = JSON.stringify(movies);
+
+// console.log(typeof stringifiedMovies);
+
+// try {
+//   const parsedMovies = JSON.parse(stringifiedMovies);
+
+//   console.log('Is valid', parsedMovies);
+// } catch (error) {
+//   console.error('Is invalid', error);
+// }
